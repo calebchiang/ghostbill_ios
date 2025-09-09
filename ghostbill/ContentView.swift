bc100f766afe56a68400bf1761f41ea6a7522e79
Caleb Chiang <calebchiang1002@gmail.com>
2025-09-09 11:36:20 -0700

    Initial Commit

---
+++ ghostbill/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ghostbill
+//
+//  Created by Caleb Chiang on 2025-09-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ghostbill/ghostbillApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghostbillApp.swift
+//  ghostbill
+//
+//  Created by Caleb Chiang on 2025-09-09.
+//
+
+import SwiftUI
+
+@main
+struct ghostbillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
